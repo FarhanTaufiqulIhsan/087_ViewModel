be4fee2388b1b5b9fab4e672a6bc2d741c924030
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-10-27 14:20:33 +0700

    Mengubah letak dari value "sudah" dan "belum" ke variable stats

--- app/src/main/java/com/example/formdata/DataSource.kt
+++ app/src/main/java/com/example/formdata/DataSource.kt
@@ -4,7 +4,9 @@ object DataSource {
     val jenis = listOf(
         R.string.laki,
         R.string.wanita,
+    )
+    val stats = listOf(
         R.string.belum,
-        R.string.sudah
+        R.string.sudah,
     )
 }
