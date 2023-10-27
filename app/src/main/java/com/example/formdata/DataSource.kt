ea277300844105f4ad0b616cb00564d65df416ba
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-10-27 14:17:40 +0700

    Menambahkan string menikah dan belum menikah, serta menambahkan variable mar untuk status

--- app/src/main/java/com/example/formdata/DataSource.kt
+++ app/src/main/java/com/example/formdata/DataSource.kt
@@ -3,6 +3,8 @@ package com.example.formdata
 object DataSource {
     val jenis = listOf(
         R.string.laki,
-        R.string.wanita
+        R.string.wanita,
+        R.string.belum,
+        R.string.sudah
     )
 }
